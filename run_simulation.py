cfaaa76b260b7d7d968b3206dc17755465bafbc0
Taylor Cavazos <taylorcavazos@ucsfs-mbp-8.lan.ucsf.edu>
2020-04-07 14:41:11 -0700

    option for parallel processing added to run

--- run_simulation.py
+++ run_simulation.py
@@ -3,7 +3,7 @@ from config import *
 import argparse
 import os
 
-def main(sim,m,h2,weight,snp,p,r2,cases_yri,outdir):
+def main(sim,m,h2,weight,snp,p,r2,cases_yri,outdir,threads):
 	# PART 0: CREATE DIRECTORIES NEEDED FOR SIMULATION
 	os.system(f"mkdir -p {outdir}sim{sim}"+"/{trees,admixed_data/{input,output},true_prs,emp_prs,summary}")
 	
@@ -29,7 +29,7 @@ def main(sim,m,h2,weight,snp,p,r2,cases_yri,outdir):
 	## optional parameters which can be modified: p-value, r2, weighting, snp-selection (future),
 	## number of yri samples to be used as training
 	SIM.create_emp_prs(m, h2, N_ADMIX, f"{outdir}sim{sim}/", p, r2,snp_weighting=weight,
-						snp_selection=snp,num2decrease=cases_yri)
+						snp_selection=snp,num2decrease=cases_yri,num_threads=threads)
 
 	# PART 4: SUMMARIZE RESULTS
 	SIM.output_all_summary(sim,m,h2,f"{outdir}sim{sim}/", p, r2, snp_weighting=weight,
@@ -48,9 +48,10 @@ parser.add_argument("--pvalue",help="pvalue cutoff to be used for LD clumping", 
 parser.add_argument("--ld_r2",help="r2 cutoff to be used for LD clumping", type=float, default=R2)
 parser.add_argument("--decrease_samples_yri",help="# of cases used in YRI analysis to represent the lack of non-European data", type=int, default=None)
 parser.add_argument("--output_dir",help="location for output data to be written", type=str, default="output/")
+parser.add_argument("--threads",help="# of threads to use for parallel processing", type=int, default=8)
 
 args = parser.parse_args()
 if args.output_dir[-1]!="/": args.output_dir+="/"
 main(args.sim,args.m,args.h2,args.snp_weighting,
 	args.snp_selection,args.pvalue,args.ld_r2,
-	args.decrease_samples_yri,args.output_dir)
+	args.decrease_samples_yri,args.output_dir,args.threads)
